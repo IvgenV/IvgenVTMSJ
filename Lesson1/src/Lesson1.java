0a9b959685f8f8fe1939f4a87702ff24d92db432
Ivgen <ivgenij.v@gmail.com>
2020-11-27 19:16:15 +0300

    messege

--- Lesson1/src/Lesson1.java
+++ Lesson1/src/Lesson1.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class Lesson1 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
 
-        boolean chose = true;
-        boolean chse2 = true;
+        boolean contin;
+        boolean exit;
         int cs;
         String vv;
         Scanner scanner = new Scanner(System.in);
 
         do {
-
-            System.out.print("Выбирете задание от 1 до 7:");
+            contin = true;
+            exit = true;
+            System.out.print("Выбирете задание от 1 до 7: ");
             cs = scanner.nextInt();
             switch (cs){
                 case 1:
@@ -47,89 +48,90 @@ public class Lesson1 {
             }
 
             do {
-                System.out.print("Желаете продолжить Да/Нет ?");
-                vv = scanner.nextLine();
-                if(vv.equals("Да")){
-                    chse2 = false;
-                } else if (vv.equals("Нет")){
-                    chose = false;
-                    chse2  = false;
-                } else System.out.print("Шьто я не поняль?");
-            }while (chse2);
+                System.out.print("Желаете продолжить Yex/No? ");
+                vv = scanner.next();
 
-        } while (chose);
+                if(vv.equalsIgnoreCase("yes")){
+                    exit = false;
+                } else if (vv.equalsIgnoreCase("no")){
+                    contin = false;
+                    exit  = false;
+                } else System.out.print("Шьто я не поняль? Давай по новой ");
+            }while (exit);
+
+        } while (contin);
+        scanner.close();
     }
 
     public static void getCountofDigits(){
         int count;
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите число:");
+        System.out.print("Введите число: ");
         long num = in.nextLong();
 
         if(num>0){
             count = (int)(Math.log10(num)+1);
             switch (count) {
                 case 1 ->
-                    System.out.print("Колличество цифр в числе: " + count + ", число положительное однозначное.");
+                    System.out.println("Колличество цифр в числе: " + count + ", число положительное однозначное.");
 
                 case 2 ->
-                    System.out.print("Колличество цифр в числе: " + count + ", число положительное двухзначное.");
+                    System.out.println("Колличество цифр в числе: " + count + ", число положительное двухзначное.");
 
                 default ->
-                    System.out.print("Колличество цифр в числе: " + count + ", число положительное трехзначное и более");
+                    System.out.println("Колличество цифр в числе: " + count + ", число положительное трехзначное и более");
 
             }
         } else if(num==0){
             count = 1;
-            System.out.print("Колличество цифр в числе: " + count + ", число ноль");
+            System.out.println("Колличество цифр в числе: " + count + ", число ноль");
         } else {
             count = (int)(Math.log10(Math.abs(-num))+1);
             switch (count) {
                 case 1 ->
-                        System.out.print("Колличество цифр в числе: " + Math.abs(-count) + ", число отрицательное однозначное");
+                        System.out.println("Колличество цифр в числе: " + Math.abs(-count) + ", число отрицательное однозначное");
                 case 2 ->
-                        System.out.print("Колличество цифр в числе: " + Math.abs(-count) + ", число отрицательное двухзначное");
+                        System.out.println("Колличество цифр в числе: " + Math.abs(-count) + ", число отрицательное двухзначное");
                 default ->
-                        System.out.print("Колличество цифр в числе: " + Math.abs(-count) + ", число отрицательное трехзначное " +
+                        System.out.println("Колличество цифр в числе: " + Math.abs(-count) + ", число отрицательное трехзначное " +
                                 "и более");
             }
         }
-        in.close();
     }
 
     public static void triangle(){
         int a,b,c;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите первое число:");
+        System.out.println("Введите первое число: ");
         a = scanner.nextInt();
-        System.out.print("Введите второе число:");
+        System.out.println("Введите второе число: ");
         b = scanner.nextInt();
-        System.out.print("Введите третье число:");
+        System.out.println("Введите третье число: ");
         c = scanner.nextInt();
 
         if (a+b<c){
-            System.out.print("Это не треугольник!");
+            System.out.println("Это не треугольник!");
         } else if(b+c<a){
-            System.out.print("Это не треугольник!");
+            System.out.println("Это не треугольник!");
         } else if (a+c<b){
-            System.out.print("Это не треугольник!");
-        } else System.out.print("Да это он");
+            System.out.println("Это не треугольник!");
+        } else System.out.println("Да это он");
 
     }
 
     public static void number(){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите число:");
+        System.out.print("Введите число: ");
         int number = scanner.nextInt();
         if(number>0){
             number++;
-            System.out.print("Число после преоброзований:" + number);
+            System.out.println("Число после преоброзований:" + number);
         }else if(number == 0){
             number = 10;
-            System.out.print("Число после преоброзований:" + number);
+            System.out.println("Число после преоброзований:" + number);
         } else {
             number-=2;
-            System.out.print("Число после преоброзований:" + number);
+            System.out.println("Число после преоброзований:" + number);
         }
     }
 
@@ -137,11 +139,11 @@ public class Lesson1 {
         int a,b,c;
         int sum=0;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите превое число:");
+        System.out.println("Введите превое число: ");
         a = scanner.nextInt();
-        System.out.print("Введите второе число:");
+        System.out.println("Введите второе число: ");
         b = scanner.nextInt();
-        System.out.print("Введите третье число:");
+        System.out.println("Введите третье число: ");
         c = scanner.nextInt();
         int[] map = {a,b,c};
         for(int i = 0;i<3;i++){
@@ -149,7 +151,7 @@ public class Lesson1 {
                 sum++;
             }
         }
-        System.out.print("Количество положительных чисел: " + sum);
+        System.out.println("Количество положительных чисел: " + sum);
     }
 
     public static void positiveAndNegative(){
@@ -157,11 +159,11 @@ public class Lesson1 {
         int positive = 0;
         int negative = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите превое число:");
+        System.out.println("Введите превое число: ");
         a = scanner.nextInt();
-        System.out.print("Введите второе число:");
+        System.out.println("Введите второе число: ");
         b = scanner.nextInt();
-        System.out.print("Введите третье число:");
+        System.out.println("Введите третье число: ");
         c = scanner.nextInt();
         int[] map = {a,b,c};
         for(int i = 0;i<3;i++){
@@ -171,7 +173,7 @@ public class Lesson1 {
                 negative++;
             }
         }
-        System.out.print("Количество положительных чисел: " + positive + "\n" +
+        System.out.println("Количество положительных чисел: " + positive + "\n" +
                 "Количество отрицательных: " + negative);
 
     }
@@ -179,28 +181,28 @@ public class Lesson1 {
     public static void biggestone(){
         int a,b;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите первое число:");
+        System.out.println("Введите первое число: ");
         a = scanner.nextInt();
-        System.out.print("Введите второе число:");
+        System.out.println("Введите второе число: ");
         b = scanner.nextInt();
         if (a > b) {
-            System.out.print(a + " больше");
-        }else System.out.print(b + " больше");
+            System.out.println(a + " больше");
+        }else System.out.println(b + " больше");
 
     }
 
     public static void programists(){
         int a;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите число программистов:");
+        System.out.println("Введите число программистов: ");
         a = scanner.nextInt();
         if(a>=5 && a<=20 || (a%10 >=5 && a%10 <=9) || a%10==0 ){
-            System.out.print("В вашей комманде " + a + " программистов.");
+            System.out.println("В вашей комманде " + a + " программистов.");
         }
         else if(a%10>=2 && a%10<=4){
-            System.out.print("В вашей комманде " + a + " программиста.");
+            System.out.println("В вашей комманде " + a + " программиста.");
         } else {
-            System.out.print("В вашей комманде " + a + " программист.");
+            System.out.println("В вашей комманде " + a + " программист.");
         }
 
     }
